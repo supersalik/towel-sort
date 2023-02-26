692f91b15e7d10b1a05dd83fccb393568cd1509e
Dzmitry Salivon <saleevong@gmail.com>
2023-02-26 12:25:08 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (matrix === undefined) return [];
+    else
+        return matrix.flat().sort((a, b) => {
+            if (a > b) return 1;
+            if (a < b) return -1;
+        });
+};
